1be2b759fd7a0150e5f65abd6e477fc1d7caa6ba
Stefan Fambach <spam1@bobbelsche.de>
2022-04-18 21:09:38 +0200

    sgp30 shield

--- shields/SGP30/examples/SimpleSensorTest/SimpleSensorTest.ino
+++ shields/SGP30/examples/SimpleSensorTest/SimpleSensorTest.ino
@@ -1,12 +1,24 @@
+/** D1 mini Shiel - SGP30 Sensor 
+ *  
+ *  Lib:
+ *  https://github.com/adafruit/Adafruit_SGP30
+ *  
+ *  Created with help of the existing examples
+ *  Created by S. Fambach
+ *  
+ *  Visit https://www.Fambach.net if you want
+ *  
+ */
+
 #include <Wire.h>
 #include <Adafruit_SGP30.h>
-Adafruit_SGP30 sgp30; 
+Adafruit_SGP30 sensor; 
 
 void setup()
 {
 
   Serial.begin(115200);
-  if (sgp30.begin() == false)
+  if (sensor.begin() == false)
   {
     Serial.println(F("Sensor not found"));
     while (1)
@@ -21,14 +33,14 @@ void loop()
 
   delay(1000);
 
-  if (sgp30.IAQmeasure())
+  if (sensor.IAQmeasure())
   {
 
     Serial.print("CO2: ");
-    Serial.println(sgp30.eCO2);
+    Serial.println(sensor.eCO2);
 
     Serial.print("TVOC: ");
-    Serial.println(sgp30.TVOC);
+    Serial.println(sensor.TVOC);
   }
   else
   {
